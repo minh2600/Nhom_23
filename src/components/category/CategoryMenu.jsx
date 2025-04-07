3ad58dd8d316313db77f2e6240bc3ed4538ef6b3
minh2600 <nguyenminh00.2003@gmail.com>
2025-04-07 23:02:33 +0700

    Header Components

--- src/components/category/CategoryMenu.jsx
+++ src/components/category/CategoryMenu.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import { Menu } from "antd";
 import styles from "./CategoryMenu.module.css";
 import { Link } from "react-router-dom";
-import CategoryEvents from "../../pages/user/category_events/CategoryEvents";
+
 const CategoryMenu = () => {
     const menuItems = [
-        { key: "music", label: <Link to ={"/music"}>Âm nhạc</Link>, },
+        { key: "music", label: <Link to={"/music"}>Âm nhạc</Link>, },
         { key: "art", label: "Sân khấu & Nghệ thuật" },
         { key: "sports", label: "Thể thao" },
         { key: "others", label: "Khác" },
